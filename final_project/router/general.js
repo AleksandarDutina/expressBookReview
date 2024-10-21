d02f08ae30185c28fff4bb2530516b9089fc54d9
Luka Dutina <dutinaluk774@gmail.com>
2024-10-21 20:55:20 +0900

    get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,8 +47,17 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const titleName = req.params.title;
+  const bookKeys = Object.keys(books);
+
+  const booksByTitle = bookKeys
+    .map((key) => books[key])
+    .filter((book) => book.title.toLowerCase() === titleName.toLowerCase());
+
+  if (booksByTitle.length === 0) {
+    return res.status(404).json({ message: "No books found with this title" });
+  }
+  return res.status(200).json({ books: booksByTitle });
 });
 
 //  Get book review
